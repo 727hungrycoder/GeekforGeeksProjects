5fa5365dc18df92ab39ef8b6aeee4e0635fd7c03
Ankita <46222639+727hungrycoder@users.noreply.github.com>
2024-01-01 23:45:38 +0100

    Compound_Interest

---
+++ compund_interest.py
@@ -0,0 +1,68 @@
+#def compound_intetrest(p,t,r,):
+   # print("Enter the principal",p)
+   # print("Enter the time",t)
+   # print("Enter the rate",r)
+    
+    
+#    Amount = p * (pow((1 + r/100),t))
+    
+#    compound_intetrest = Amount - p
+    
+#    print("Compound_Interest is",compound_intetrest)
+    
+    
+#compound_intetrest(100000,4,5)
+
+
+
+#def compound_intetrest(p,t,r):
+   # print("Enter the principal",p)
+    #print("Enter the time",t)
+    #print("Enter the rate",r)
+    #
+    
+#    Amount = p * (pow((1 + r/100),t))
+    
+#    compound_intetrest = Amount - p
+    
+#    print("Compound_Interest is",compound_intetrest)
+    
+ 
+#p = int(input("Enter the principal"))
+#t = int(input("Enter the rate"))
+#r = int(input("Enter the time#"))
+    
+#compound_intetrest(p,t,r)
+
+
+
+#p = 1200
+#t = 2
+#r = 5
+
+#a = p*((1+r/100)**t)
+
+
+#ci = a-p
+
+#print(ci)
+
+
+
+
+
+#def compound_interest(p,r,t):
+    #Amount = p
+#    for i in range(t):
+#        Amount = Amount * (1 + r/100)
+        
+#    CI = Amount - p
+#    print(CI) 
+    
+#compound_interest(10000,2,2)   
+
+
+
+    
+
+
