8f6ecbfc0bdc8d4b352a194683092469e28a0f3e
727hungrycoder <727.ankita@gmail.com>
2024-01-16 16:29:00 +0100

    1-16-2024

--- to_do_list.py
+++ to_do_list.py
@@ -1,10 +1,12 @@
 tasks = []
 
+
 def addTask():
     task = input("Please enter a task")
     tasks.append(task)
-    print(f"Tasks {task} added to the list.")    
-    
+    print(f"Tasks {task} added to the list.")
+
+
 def listTasks():
     if not tasks:
         print("There are no task currently.")
@@ -12,49 +14,44 @@ def listTasks():
         print("Current tasks:")
         for index, task in enumerate(tasks):
             print(f"Task #{index}. {task}")
-            
-            
+
+
 def deleteTask():
     listTasks()
     try:
         tasktoDelete = int(input("Enter the task # to delete"))
-        if tasktoDelete >=0 and tasktoDelete<len(tasks):
+        if tasktoDelete >= 0 and tasktoDelete < len(tasks):
             tasks.pop(tasktoDelete)
             print(f"Task {tasktoDelete} has een removed")
         else:
             print(f"Task {tasktoDelete} was not found")
     except:
         print("Invalid input")
-        
-            
-            
+
 
 if __name__ == "__main__":
-    ## Create a loop to run the app
+    # Create a loop to run the app
     print("Welcome to the to do list app")
-    
+
     while True:
-        #print("\n")
+        # print("\n")
         print("Please select one of the following options")
         print("------------------------------------------")
         print("1. Add a new task")
         print("2. Delete a task")
         print("3. List the tasks")
         print("4. Quit")
-        
-        
-        choice =input("Enter your choice")
-        
-        
-        if choice=="1":
+
+        choice = input("Enter your choice")
+
+        if choice == "1":
             addTask()
-        elif choice=="2":
-            deleteTask()        
-        elif choice =="3":
+        elif choice == "2":
+            deleteTask()
+        elif choice == "3":
             listTasks()
-        elif choice =="4":
+        elif choice == "4":
             break
         else:
             print("Invalid Input")
-    print("Good bye")        
-        
+    print("Good bye")
